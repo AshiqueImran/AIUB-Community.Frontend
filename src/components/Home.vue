fceaba043f252827e1a5fa55df9a869a11e9a8e5
Raihan Al-Mamun <iam.raihan96@gmail.com>
2017-11-24 22:32:06 +0600

    download link added, deploy for testing

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,7 +19,7 @@
               outline round large
               color="info"
               target="_blank"
-              href="https://www.google.com">
+              href="https://s3.ap-south-1.amazonaws.com/aiubcommunity/MMS-Installer.exe">
               <v-icon left>file_download</v-icon>
               Download v1.0&nbsp;
             </v-btn>
